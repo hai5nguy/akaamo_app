c4279cf6f4e4a9c801470697939f31f19de89b07
Anthony Natoli <a.natoli@salesforce.com>
2017-10-19 12:10:57 -0400

    Initial UI work

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,21 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.s?css$/,
+        use: [
+            {
+                loader: "style-loader" // creates style nodes from JS strings
+            },
+            {
+                loader: "css-loader" // translates CSS into CommonJS
+            },
+            {
+                loader: "sass-loader" // compiles Sass to CSS
+            }
+        ]
+      }
     ]
   }
 }
