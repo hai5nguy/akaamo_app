3d56b5686515c7f6fc9b558d8e46a3ca1db1b8b0
Hai Nguyen <hai5nguy@gmail.com>
2017-10-19 16:16:17 -0400

    More work on event panel

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,16 +1,27 @@
 import React from 'react';
 
+import EventPanel from './event-panel.js';
+
 import '../styles/home.scss';
 
 class Home extends React.Component {
 	render () {
+		var data = {
+			name: 'Debutante Cotillion and Scholarship Program',
+			startDate: '2017-11-12T16:00:00-05:00',
+			endDate: '2017-11-12T19:00:00-05:00',
+			location: 'Indiana Roof Ballroom'
+		};
+
 		return (
 			<div className="home">
 				<div className="content-header">Home</div>
-				<div className="home-logo">logo</div>
-				<div className="content-body">
-					<div className="about-text">This is the home page</div>
-				</div>
+				<div className="home-logo"></div>
+				<EventPanel data={data} />
+				<EventPanel data={data} />
+				<EventPanel data={data} />
+				<EventPanel data={data} />
+				<EventPanel data={data} />
 			</div>
 		);
 	}	
