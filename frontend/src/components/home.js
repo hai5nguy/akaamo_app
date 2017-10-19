81ec07f922bd4643dda226d51194d8696708551c
Hai Nguyen <hai5nguy@gmail.com>
2017-10-19 15:24:32 -0400

    Update home view

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
+import '../styles/home.scss';
+
 class Home extends React.Component {
 	render () {
-		return (<div className="about-us">
-		<div className="content-header">Home</div>
-		<div className="content-body">
-			<div className="about-text">This is the home page</div>
-		</div>
-	</div>);
+		return (
+			<div className="home">
+				<div className="content-header">Home</div>
+				<div className="home-logo">logo</div>
+				<div className="content-body">
+					<div className="about-text">This is the home page</div>
+				</div>
+			</div>
+		);
 	}	
 }
 
